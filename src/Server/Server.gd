9256bd2f03440cbbbe7898a4272a21ad5689b832
Fercho Posting <shaaf4ever@gmail.com>
2021-11-29 21:12:50 -0600

    Listen

--- src/Server/Server.gd
+++ src/Server/Server.gd
@@ -24,7 +24,7 @@ func client():
 func server():
 	print("Server listening")
 	var peer = NetworkedMultiplayerENet.new()
-	peer.set_bind_ip('0.0.0.0')
+	peer.set_bind_ip('134.122.117.174')
 	peer.create_server(6969, 10)
 	get_tree().network_peer = peer
 
